690955a70ad2b7456b253c0871fd9f6d41fa51c7
matymcleod <matymcleod@gmail.com>
2021-06-30 03:02:12 +0000

    added questions and answers to survey, also tested and confirmed working

--- survey.js
+++ survey.js
@@ -4,9 +4,15 @@ const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
-
-rl.question('What do you think of Node.js? ', (answer) => {
-  console.log(`Thank you for your valuable feedback: ${answer}`);
-
-  rl.close();
+let survey = [];
+rl.question('What is you name? ', (name) => {
+  survey.push(`You answered that your name is: ${name}`)
+    rl.question('How old are you? ', (answer) => {
+      survey.push(`You are: ${answer}`)
+        rl.question('Do you like participating in surveys? ', (answer => {
+          survey.push(`You said that you ${answer} like participating in surveys`)
+            console.log(`Thank you for your valuable feedback ${name}, here are your responses: ${survey}`);
+              rl.close();
+        }))
+    })
 });
